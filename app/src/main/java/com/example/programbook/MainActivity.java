ff6d53eb0d3ab88949a972d995bd03e93f872d24
dikhaarianda <dikhaarianda@gmail.com>
2022-09-29 22:09:11 +0700

    delete toast

--- app/src/main/java/com/example/programbook/MainActivity.java
+++ app/src/main/java/com/example/programbook/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.programbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/programbook/RegisterActivity.java
+++ app/src/main/java/com/example/programbook/RegisterActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
