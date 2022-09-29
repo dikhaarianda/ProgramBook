2e70388783d14ab57b833d2125daa440d51c675c
Joo_Kids <yonathanjo24@gmail.com>
2022-09-30 01:21:31 +0700

    Update bagian Register, dikasih checkbox sama If else tapi gatau beneran bisa running atau nggak, di gua gabisa testing

--- app/src/main/java/com/example/programbook/RegisterActivity.java
+++ app/src/main/java/com/example/programbook/RegisterActivity.java
@@ -4,13 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
     userDatabase db;
     EditText name, userName, email, password;
-    Button login2;
+    Button CreateAcc;
+    CheckBox Agreement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +20,22 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         db = new userDatabase(this);
-        login2 = findViewById(R.id.btn_login2);
+        CreateAcc = findViewById(R.id.btn_Register);
         name = findViewById(R.id.txt_name);
         userName = findViewById(R.id.txt_user);
         email = findViewById(R.id.txt_email);
         password = findViewById(R.id.txt_pass2);
 
-        login2.setOnClickListener(view -> {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        });
+        if (Agreement.isChecked()) {
+            CreateAcc.setOnClickListener(view -> {
+                Intent intentBetul = new Intent(this, LoginActivity.class);
+                startActivity(intentBetul);
+            });
+        }
+        else
+            CreateAcc.setOnClickListener(view -> {
+                Intent intentgagal = new Intent(this, RegisterActivity.class);
+                startActivity(intentgagal);
+            });
     }
 }
