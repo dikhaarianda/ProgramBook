ab891c4c4d21531ebdcb8e1f82db26a27f99238f
dikhaarianda <dikhaarianda@gmail.com>
2022-09-29 22:00:00 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/programbook/MainActivity.java
+++ app/src/main/java/com/example/programbook/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this,"test",Toast.LENGTH_LONG).show();
         Toast.makeText(MainActivity.this,"joo",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this,"Data ini cuman test",Toast.LENGTH_LONG).show();
     }
 }
