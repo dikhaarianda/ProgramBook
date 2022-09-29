a0b3e77521e7c65733ada0b1725b7bdfbfbc611f
Joo_Kids <yonathanjo24@gmail.com>
2022-09-30 00:49:39 +0700

    Update Login Activity Bagian Text Register

--- app/src/main/java/com/example/programbook/LoginActivity.java
+++ app/src/main/java/com/example/programbook/LoginActivity.java
@@ -5,21 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     Button login;
     EditText username;
+    TextView Register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         username = findViewById(R.id.txt_login);
         login = findViewById(R.id.btn_login);
         login.setOnClickListener(view -> {
-            Intent intent = new Intent(this, MainActivity.class);
+            Intent intent_login = new Intent (this, MainActivity.class);
             String users = username.getText().toString();
-            intent.putExtra("Username", users);
-            startActivity(intent);
+            intent_login.putExtra("Username", users);
+            startActivity(intent_login);
+        });
+
+        Register = findViewById(R.id.textViewCreateAccount);
+        Register.setOnClickListener(view -> {
+            Intent Intentregister = new Intent(this, RegisterActivity.class);
+            startActivity(Intentregister);
         });
     }
 }
