4f5bc2c64268584049edd6dc299722c93bdab811
Joo_Kids <yonathanjo24@gmail.com>
2022-09-29 10:48:08 +0700

    update MainActivity
    Update anjeng

--- app/src/main/java/com/example/programbook/MainActivity.java
+++ app/src/main/java/com/example/programbook/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this,"test",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this,"joo",Toast.LENGTH_LONG).show();
     }
 }
